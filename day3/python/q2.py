9e94f163bf4fee419cb1a171fb6ecd07ff74ba14
Alan Silva <alanfortlink@gmail.com>
2024-12-04 02:56:56 +0000

    Update

--- day3/python/q2.py
+++ day3/python/q2.py
@@ -2,32 +2,11 @@ import sys
 import re
 
 content = open(sys.argv[1], 'r').read()
-enabled = True
-
-INF = len(content) + 10
-
-events = []
-regexes = [r"mul\([0-9][0-9]*,[0-9][0-9]*\)", r"don't\(\)", r"do\(\)"]
-
-for r in regexes:
-    items = [(item.span()[0], item.group())
-             for item in re.compile(r).finditer(content)]
-    events.extend(items)
-
-events.sort(key=lambda x: x[0])
+regexes = [r"mul\(([0-9][0-9]*),([0-9][0-9]*)\)", r"don't\(\)", r"do\(\)"]
 enabled = True
 total = 0
-for e in events:
-    if "do()" in e[1]:
-        enabled = True
-        continue
-    if "don't()" in e[1]:
-        enabled = False
-        continue
-
-    if enabled:
-        inner = e[1].replace("mul(", "").replace(")", "").split(",")
-        lhs, rhs = int(inner[0]), int(inner[1])
-        total += lhs * rhs
-
+for m in re.compile("|".join(regexes)).finditer(content):
+    enabled = (enabled or "do()" in m.group()) and ("don't" not in m.group())
+    if "mul" in m.group() and enabled:
+        total += int(m.group(1)) * int(m.group(2))
 print(total)
