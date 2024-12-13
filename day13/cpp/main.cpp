d0ef8e0ad534f8d9198dc3601bbad1d804970afa
Alan Silva <alanfortlink@gmail.com>
2024-12-13 23:18:11 +0000

    Update

--- day13/cpp/main.cpp
+++ day13/cpp/main.cpp
@@ -2,17 +2,16 @@
 #include <cstdio>
 #include <fstream>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 typedef pair<int64_t, int64_t> coord;
-const int64_t INC = 10000000000000;
-
-coord get_prize_coord(const string &encoded_prize) {
+coord get_prize_coord(const string &encoded_prize, int64_t inc) {
   coord c;
-  sscanf(encoded_prize.c_str(), "Prize: X=%lld, Y=%lld", &c.first, &c.second);
-  c.first += INC;
-  c.second += INC;
+  sscanf(encoded_prize.c_str(), "Prize: X=%ld, Y=%ld", &c.first, &c.second);
+  c.first += inc;
+  c.second += inc;
   return c;
 }
 
@@ -24,7 +23,7 @@ struct instruction {
   int64_t y() const { return delta.second; }
 
   instruction(const string &encoded_instruction) {
-    sscanf(encoded_instruction.c_str(), "Button %c: X+%lld, Y+%lld", &button,
+    sscanf(encoded_instruction.c_str(), "Button %c: X+%ld, Y+%ld", &button,
            &delta.first, &delta.second);
   }
 
@@ -36,15 +35,18 @@ struct machine {
   const instruction b;
   const coord p;
 
-  machine(const vector<string> &encoded_instructions)
+  int64_t x() const { return p.first; };
+  int64_t y() const { return p.second; };
+
+  machine(const vector<string> &encoded_instructions, int64_t inc)
       : a(instruction(encoded_instructions[0])),
         b(instruction(encoded_instructions[1])),
-        p(get_prize_coord(encoded_instructions[2])) {}
+        p(get_prize_coord(encoded_instructions[2], inc)) {}
 };
 
-int64_t q2(const machine &m) {
+int64_t count(const machine &m) {
   int64_t x_den = (m.a.x() * m.b.y() - m.b.x() * m.a.y());
-  int64_t x_num = (m.p.first * m.b.y() - m.b.x() * m.p.second);
+  int64_t x_num = (m.x() * m.b.y() - m.b.x() * m.y());
 
   int64_t x = x_num / x_den;
   int64_t xr = x_num % x_den;
@@ -54,7 +56,7 @@ int64_t q2(const machine &m) {
   }
 
   int64_t y_den = m.b.y();
-  int64_t y_num = (m.p.second - m.a.y() * x);
+  int64_t y_num = (m.y() - m.a.y() * x);
 
   int64_t y = y_num / y_den;
   int64_t yr = y_num % y_den;
@@ -67,20 +69,24 @@ int64_t q2(const machine &m) {
 }
 
 int main(int argc, char **argv) {
-  fstream fs(argc == 2 ? argv[1] : "../example.txt");
+  fstream fs(argc >= 2 ? argv[1] : "../example.txt");
   vector<string> encoded_instructions;
   int64_t total = 0;
+
+  const bool is_q2 = true;
+  int64_t inc = is_q2 ? 10'000'000'000'000 : 0;
+
   for (string line; getline(fs, line);) {
     if (!line.empty()) {
       encoded_instructions.push_back(line);
       continue;
     }
 
-    total += q2(encoded_instructions);
+    total += count(machine(encoded_instructions, inc));
     encoded_instructions.clear();
   }
 
-  total += q2(encoded_instructions);
+  total += count(machine(encoded_instructions, inc));
 
   cout << total << endl;
 }
